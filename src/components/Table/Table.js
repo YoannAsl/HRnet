7638655884209717f0aa8c3f45597af420d4cbf6
YoannAsl <47599801+YoannAsl@users.noreply.github.com>
2021-05-23 11:10:58 +0200

    test

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -9,7 +9,7 @@ import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
 import GlobalFilter from './GlobalFilter';
-import { COLUMNS } from './COLUMNS';
+import { COLUMNS } from './columns';
 
 const SearchContainer = styled.div`
 	width: 80%;
